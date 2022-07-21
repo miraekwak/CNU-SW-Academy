030d40495e0644bab213155169c0c33b38216f9a
GwakMirae <alfo9490@naver.com>
2022-07-21 16:58:02 +0900

    [feat] Create Service Structure and Apply ApplicationContext

---
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/OrderItem.java
@@ -0,0 +1,27 @@
+package org.prgrms.kdt;
+
+import java.util.UUID;
+
+public class OrderItem {
+    public final UUID productId;
+    public final long productPrice;
+    public final Long quantity;
+
+    public OrderItem(UUID productId, long productPrice, Long quantity) {
+        this.productId = productId;
+        this.productPrice = productPrice;
+        this.quantity = quantity;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public long getProductPrice() {
+        return productPrice;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+}
---
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/OrderStatus.java
@@ -0,0 +1,11 @@
+package org.prgrms.kdt;
+
+public enum OrderStatus {
+    ACCEPTED,
+    PAYMENT_REQUIRED,
+    PAYMENT_REJECTED,
+    READY_FOR_DELIVERY,
+    SHIPTED,
+    SETTLED,
+    CANCELLED
+}
