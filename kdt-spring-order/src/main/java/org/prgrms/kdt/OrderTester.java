0b2ec075cb94a88cf9c26469b94901b69af523f8
GwakMirae <alfo9490@naver.com>
2022-07-26 21:38:52 +0900

    [feat] Test Resource

--- kdt-spring-order/src/main/java/org/prgrms/kdt/OrderTester.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/OrderTester.java
@@ -11,13 +11,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.util.Assert;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class OrderTester {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(AppConfiguration.class);
         var environment = applicationContext.getEnvironment();
@@ -34,10 +40,24 @@ public class OrderTester {
 //        System.out.println(MessageFormat.format("supportVendors -> {0}", supportVendors));
 //        System.out.println(MessageFormat.format("description -> {0}", description));
         var orderProperties = applicationContext.getBean(OrderProperties.class);
-        System.out.println(MessageFormat.format("version -> {0}", orderProperties.getVersion()));
-        System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", orderProperties.getMinimumOrderAmount()));
-        System.out.println(MessageFormat.format("supportVendors -> {0}", orderProperties.getSupportVendors()));
-        System.out.println(MessageFormat.format("description -> {0}", orderProperties.getDescription()));
+//        System.out.println(MessageFormat.format("version -> {0}", orderProperties.getVersion()));
+//        System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", orderProperties.getMinimumOrderAmount()));
+//        System.out.println(MessageFormat.format("supportVendors -> {0}", orderProperties.getSupportVendors()));
+//        System.out.println(MessageFormat.format("description -> {0}", orderProperties.getDescription()));
+
+        var resource = applicationContext.getResource("classpath:application.yaml");
+        var resource2 = applicationContext.getResource("file:test/sample.txt"); // working directory 기준
+        var resource3 = applicationContext.getResource("https://stackoverflow.com/");
+
+        System.out.println(MessageFormat.format("Resource -> {0}", resource.getClass().getCanonicalName()));
+        var strings = Files.readAllLines(resource2.getFile().toPath());
+        System.out.println(strings.stream().reduce("", (a,b) -> a+"\n"+b));
+
+        System.out.println(MessageFormat.format("Resource3 -> {0}", resource3.getClass().getCanonicalName()));
+        var readableByteChannel = Channels.newChannel(resource3.getURL().openStream());
+        var bufferedReader = new BufferedReader(Channels.newReader(readableByteChannel, StandardCharsets.UTF_8));
+        var contents = bufferedReader.lines().collect(Collectors.joining("\n"));
+        System.out.println(contents);
 
         var customerId = UUID.randomUUID();
 
