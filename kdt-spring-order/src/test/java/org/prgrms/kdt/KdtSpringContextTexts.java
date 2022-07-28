1c9850ba0dac1180b4d6bb73411c7dea890b756f
GwakMirae <alfo9490@naver.com>
2022-07-28 23:24:32 +0900

    [test] Test spring context

--- kdt-spring-order/src/test/java/org/prgrms/kdt/KdtSpringContextTexts.java
+++ kdt-spring-order/src/test/java/org/prgrms/kdt/KdtSpringContextTexts.java
@@ -1,2 +1,99 @@
-package org.prgrms.kdt;public class KdtSpringContextTexts {
+package org.prgrms.kdt;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.prgrms.kdt.order.OrderItem;
+import org.prgrms.kdt.order.OrderService;
+import org.prgrms.kdt.voucher.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+
+//@ExtendWith(SpringExtension.class)
+////@ContextConfiguration(classes = {AppConfiguration.class})
+//@ContextConfiguration
+@SpringJUnitConfig
+@ActiveProfiles("test")
+public class KdtSpringContextTexts {
+
+    @Configuration
+    @ComponentScan(
+            basePackages = {"org.prgrms.kdt.order", "org.prgrms.kdt.voucher"}
+    )
+    static class Config {
+//        @Bean
+//        VoucherRepository voucherRepository(){
+//            return new VoucherRepository() {
+//                @Override
+//                public Optional<Voucher> findById(UUID voucherId) {
+//                    return Optional.empty();
+//                }
+//
+//                @Override
+//                public Voucher insert(Voucher voucher) {
+//                    return null;
+//                }
+//            };
+//        }
+    }
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    VoucherRepository voucherRepository;
+
+
+    @Test
+    @DisplayName("application context가 생성되어야 한다.")
+    public void testApplicationContext() {
+        assertThat(context, notNullValue());
+    }
+
+    @Test
+    @DisplayName("VoucherRepository가 빈으로 등록되어 있어야 한다.")
+    public void testVoucherRepositoryCreation() {
+        var bean = context.getBean(VoucherRepository.class);
+        assertThat(bean, notNullValue());
+    }
+
+    @Test
+    @DisplayName("OrderService를 사용해서 주문을 생성할 수 있다.")
+    public void testOrderService() {
+        // given
+        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100);
+        voucherRepository.insert(fixedAmountVoucher);
+
+        // when
+        var order = orderService.createOrder(
+                UUID.randomUUID(),
+                List.of(new OrderItem(UUID.randomUUID(), 200, 1L)),
+                fixedAmountVoucher.getVoucherId());
+
+        //then
+        assertThat(order.totalAmount(), is(100L));
+        assertThat(order.getVoucher().isEmpty(), is(false));
+        assertThat(order.getVoucher().get().getVoucherId(), is(fixedAmountVoucher.getVoucherId()));
+        assertThat(order.getOrderStatus(), is(OrderStatus.ACCEPTED));
+    }
 }
