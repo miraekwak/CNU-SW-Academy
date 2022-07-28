f222fca781cb664edb94f28d6e557023eea54ea5
GwakMirae <alfo9490@naver.com>
2022-07-28 23:23:58 +0900

    [test] Test OrderService by Mock Object

--- kdt-spring-order/src/test/java/org/prgrms/kdt/order/OrderServiceTest.java
+++ kdt-spring-order/src/test/java/org/prgrms/kdt/order/OrderServiceTest.java
@@ -1,4 +1,80 @@
+package org.prgrms.kdt.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
+import org.prgrms.kdt.OrderRepository;
+import org.prgrms.kdt.OrderStatus;
+import org.prgrms.kdt.voucher.FixedAmountVoucher;
+import org.prgrms.kdt.voucher.MemoryVoucherRepository;
+import org.prgrms.kdt.voucher.VoucherRepository;
+import org.prgrms.kdt.voucher.VoucherService;
+
+import java.util.List;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+
 class OrderServiceTest {
-  
+
+    class OrderRepositoryStub implements OrderRepository {
+
+        @Override
+        public Order insert(Order order) {
+            return null;
+        }
+    }
+
+    @Test
+    void createOrder() {
+        // given
+        var voucherRepository = new MemoryVoucherRepository();
+        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100);
+        voucherRepository.insert(fixedAmountVoucher);
+        var sut = new OrderService(new VoucherService(voucherRepository), new OrderRepositoryStub());
+
+        // when
+        var order = sut.createOrder(
+                UUID.randomUUID(),
+                List.of(new OrderItem(UUID.randomUUID(), 200, 1L)),
+                fixedAmountVoucher.getVoucherId());
+
+        //then
+        assertThat(order.totalAmount(), is(100L));
+        assertThat(order.getVoucher().isEmpty(), is(false));
+        assertThat(order.getVoucher().get().getVoucherId(), is(fixedAmountVoucher.getVoucherId()));
+        assertThat(order.getOrderStatus(), is(OrderStatus.ACCEPTED));
+    }
+
+    @Test
+    @DisplayName("오더가 생성되어야 한다 (mock)")
+    void testCreateOrder() {
+        // given
+        var voucherServiceMock = mock(VoucherService.class);
+        var orderRepositoryMock =mock(OrderRepository.class);
+        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100);
+        when(voucherServiceMock.getVoucher(fixedAmountVoucher.getVoucherId())).thenReturn(fixedAmountVoucher);
+        var sut = new OrderService(voucherServiceMock, orderRepositoryMock);
+
+        // when
+        var order = sut.createOrder(
+                UUID.randomUUID(),
+                List.of(new OrderItem(UUID.randomUUID(), 200, 1L)),
+                fixedAmountVoucher.getVoucherId());
+
+        // then
+        assertThat(order.totalAmount(), is(100L));
+        assertThat(order.getVoucher().isEmpty(), is(false));
+        // 행위 관점에서 생각해야함
+        // Mock 객체가 어떤 행동을 하는지
+        // 메소드가 호출 됐는지
+        // voucher가 호출됐는지, order가 만들어졌는지, 바우처가 사용됐는 지
+        var inOrder= inOrder(voucherServiceMock, orderRepositoryMock);
+        inOrder.verify(voucherServiceMock).getVoucher(fixedAmountVoucher.getVoucherId());
+        inOrder.verify(orderRepositoryMock).insert(order);
+        inOrder.verify(voucherServiceMock).useVoucher(fixedAmountVoucher);
+    }
 }
