f0763fd8b821b9ea0f575adc644fb78cd7028dd6
GwakMirae <alfo9490@naver.com>
2022-08-01 22:35:31 +0900

    [feat] prepareStatement in JdbcCustomerRepository

--- kdt-spring-order/src/main/java/org/prgrms/kdt/JdbcCustomerRepository.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/JdbcCustomerRepository.java
@@ -14,26 +14,30 @@ public class JdbcCustomerRepository {
     private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
 
     public List<String> findNames(String name) {
-        var SELECT_SQL = "select * from customers where name = %s".formatted(name);
+        var SELECT_SQL = "select * from customers where name = ?";
         List<String> names = new ArrayList<>();
 
         try (
                 var connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "0000");
-                var statement = connection.createStatement();
-                var resultSet = statement.executeQuery(SELECT_SQL);
+                var statement = connection.prepareStatement(SELECT_SQL);
 
         )
         {
-            while (resultSet.next()) {
-                var customerName = resultSet.getString("name");
-                var customerId = UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id"));
-                var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
-                logger.info("customer id -> {}, customer name -> {}, create_at -> {}", customerId, customerName, createdAt);
-                names.add(customerName);
+            statement.setString(1, name);
+            logger.info("statement -> {}", statement);
+            try(
+                    var resultSet = statement.executeQuery();
+            ){
+                while (resultSet.next()) {
+                    var customerName = resultSet.getString("name");
+                    var customerId = UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id"));
+                    var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+                    logger.info("customer id -> {}, customer name -> {}, create_at -> {}", customerId, customerName, createdAt);
+                    names.add(customerName);
+                }
             }
-
-        } catch (SQLException e) {
-            logger.error("got error while closing connection", e);
+        } catch (SQLException throwable) {
+            logger.error("got error while closing connection", throwable);
         }
         return names;
     }
