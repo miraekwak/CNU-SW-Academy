8000f3d35805d940e8f7ba131eec746257c0c56d
GwakMirae <alfo9490@naver.com>
2022-08-22 16:03:27 +0900

    jpa project initial commit

---
+++ kdt-jpa/src/test/java/com/kdt/lecture/domain/PersistenceContextTest.java
@@ -0,0 +1,124 @@
+package com.kdt.lecture.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+@Slf4j
+@SpringBootTest
+public class PersistenceContextTest {
+
+    @Autowired
+    CustomerRepository repository;
+
+    @Autowired
+    EntityManagerFactory emf;
+
+    @BeforeEach
+    void setup() {
+        repository.deleteAll();
+    }
+
+    @Test
+    void 저장() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("Mirae");
+        customer.setLastName("Kwak");
+
+        entityManager.persist(customer);
+        transaction.commit();
+    }
+
+    @Test
+    void 조회_DB조회() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("Mirae");
+        customer.setLastName("Kwak");
+
+        entityManager.persist(customer);
+        transaction.commit();
+
+        entityManager.detach(customer);
+
+        Customer selected = entityManager.find(Customer.class, 1L);
+        log.info("{} {}", selected.getFirstName(), selected.getLastName());
+    }
+
+    @Test
+    void 조회_1차캐시_이용() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("Mirae");
+        customer.setLastName("Kwak");
+
+        entityManager.persist(customer);
+        transaction.commit();
+
+        Customer selected = entityManager.find(Customer.class, 1L);
+        log.info("{} {}", selected.getFirstName(), selected.getLastName());
+    }
+
+    @Test
+    void 수정() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("Mirae");
+        customer.setLastName("Kwak");
+
+        entityManager.persist(customer);
+        transaction.commit();
+
+        transaction.begin();
+        customer.setFirstName("Mara");
+        customer.setLastName("K");
+        transaction.commit();
+    }
+
+    @Test
+    void 삭제() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("Mirae");
+        customer.setLastName("Kwak");
+
+        entityManager.persist(customer);
+        transaction.commit();
+
+        transaction.begin();
+        entityManager.remove(customer);
+        transaction.commit()
+    }
+}
